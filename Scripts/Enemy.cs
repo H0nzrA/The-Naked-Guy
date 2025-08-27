852027c97f21c49629f9e30ad04751bc2b5cb7d7
Teddy Andrianina <tedan.h0nzra@gmail.com>
2025-08-27 22:05:11 +0300

    Add Enemy scenes | Font Title game

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Enemy : CharacterBody2D
+{
+	[Export] public float Speed = 200;
+    private AnimatedSprite2D p_sprite;
+    private string animationNode = "Enemy_animations";
+
+    public override void _Ready()
+    {
+        GD.Print("Enemy scene ready");
+        p_sprite = GetNode<AnimatedSprite2D>("Enemy_animations");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("Front"))
+        {
+            p_sprite.Animation = "Front";
+        }
+        else if (Input.IsActionPressed("Back"))
+        {
+            p_sprite.Animation = "Back";
+        }
+        else if (Input.IsActionPressed("Right"))
+        {
+            p_sprite.Animation = "Right";
+        }
+        else if (Input.IsActionPressed("Left"))
+        {
+            p_sprite.Animation = "Left";
+        }
+        else
+        {
+            p_sprite.Animation = "Idle";
+        }
+    }
+}
